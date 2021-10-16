fb1c3c361ccac029184c84684a63ef2208d4440a
Seph <57948436+vj-liboon@users.noreply.github.com>
2021-10-16 15:49:22 +0800

    Added Skeleton for Options and Code Split

---
+++ src/components/home/Options.vue
@@ -0,0 +1,192 @@
+<template>
+  <div id="options">
+    <OptionsSkeleton v-if="isLoading" />
+    <div id="optionsDefault" class="card m-1" v-else>
+      <p class="title u-text-center mb-0">Options</p>
+      <div class="u-block">
+        <div
+          class="form-ext-control form-ext-checkbox u-inline-block"
+          v-for="(value, index) in options"
+          :key="index"
+        >
+          <input
+            :id="'check' + index"
+            class="form-ext-input"
+            type="checkbox"
+            v-model="options[index]"
+            v-if="typeof options[index] == 'boolean'"
+          />
+          <label
+            class="form-ext-label"
+            :for="'check' + index"
+            v-if="typeof options[index] == 'boolean'"
+            @mouseover="optionHover = index"
+            @mouseleave="optionHover = null"
+          >
+            {{ index }}
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label
+            class="m-0"
+            for="selectResultLanguage"
+            @mouseover="optionHover = 'resultLanguage'"
+            @mouseleave="optionHover = null"
+          >
+            Result Language:
+          </label>
+          <select
+            id="selectResultLanguage"
+            class="select input-small"
+            placeholder="Choose one"
+            v-model="options.resultLanguage"
+          >
+            <option
+              v-for="language in languages"
+              :key="language"
+              :value="language"
+              >{{ language }}</option
+            >
+          </select>
+        </div>
+        <div class="col-6">
+          <label
+            class="m-0"
+            for="selectQueryLanguages"
+            @mouseover="optionHover = 'queryLanguages'"
+            @mouseleave="optionHover = null"
+          >
+            Query Languages:
+          </label>
+          <select
+            id="selectQueryLanguages"
+            class="select input-small"
+            placeholder="Choose one"
+            @change="addQueryLanguage($event)"
+          >
+            <option :value="null">-- Select --</option>
+            <option
+              v-for="language in languages"
+              :key="language"
+              :value="language"
+              >{{ language }}</option
+            >
+          </select>
+          <div class="tag-container u-center mt-2">
+            <div
+              class="tag tag--primary"
+              v-for="language in options.queryLanguages"
+              :key="language"
+            >
+              {{ language }}
+              <div
+                class="tag tag__delete mt-1"
+                @click="removeQueryLanguage(language)"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <blockquote class="mx-1 mt-1 animated fadeIn" v-if="optionHover">
+        <span
+          ><strong>{{ optionHover }}:</strong>
+          {{ optionsDesc[optionHover] }}
+        </span>
+      </blockquote>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import axios from "axios";
+
+import * as api from "@/assets/js/api.js";
+
+export default {
+  components: {
+    OptionsSkeleton: defineAsyncComponent(() =>
+      import("@/components/home/OptionsSkeleton")
+    ),
+  },
+  data() {
+    return {
+      isLoading: Boolean,
+      options: {
+        dumpResult: false,
+        matchAltNames: false,
+        matchAliases: false,
+        matchCategories: true,
+        verboseCategories: false,
+        resultLanguage: "English",
+        queryLanguages: ["English"],
+      },
+      languages: null,
+      optionHover: null,
+    };
+  },
+  mounted() {
+    this.getLanguages();
+  },
+  watch: {
+    options: {
+      deep: true,
+      immediate: true,
+      handler() {
+        this.$emit("options", this.options);
+      },
+    },
+  },
+  methods: {
+    getLanguages() {
+      this.isLoading = true;
+      axios
+        .get(api.getUrl("languages"))
+        .then((response) => (this.languages = response.data))
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+
+    addQueryLanguage(event) {
+      let language = event.target.value;
+      if (!(language == "-- Select --")) {
+        if (!this.options.queryLanguages.includes(language)) {
+          this.options.queryLanguages.push(language);
+        }
+      }
+    },
+    removeQueryLanguage(language) {
+      if (this.options.queryLanguages.includes(language)) {
+        this.options.queryLanguages.pop(language);
+      }
+    },
+  },
+  computed: {
+    optionsDesc() {
+      return {
+        dumpResult:
+          "The query result will return an object with the properties: query, folder, match, options, filename, result.",
+        matchAltNames: "Allows the matching of alternate or custom names.",
+        matchAliases:
+          "Allows the matching of aliases. These are searchable fields that returns the data object the query matched in.",
+        matchCategories:
+          "Allows the matching of categories. If true, then returns an array if it matches.",
+        verboseCategories:
+          "Used if a category is matched. If true, then replaces each string name in the array with the data object instead.",
+        queryLanguages:
+          "Array of languages that your query will be searched in.",
+        resultLanguage: "Output language that you want your results to be in.",
+      };
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,101 +61,7 @@
             </div>
           </div>
         </div>
-        <div id="options" class="card m-1">
-          <p class="title u-text-center mb-0">Options</p>
-          <div class="u-block">
-            <div
-              class="form-ext-control form-ext-checkbox u-inline-block"
-              v-for="(value, index) in options"
-              :key="index"
-            >
-              <input
-                :id="'check' + index"
-                class="form-ext-input"
-                type="checkbox"
-                v-model="options[index]"
-                v-if="typeof options[index] == 'boolean'"
-              />
-              <label
-                class="form-ext-label"
-                :for="'check' + index"
-                v-if="typeof options[index] == 'boolean'"
-                @mouseover="optionHover = index"
-                @mouseleave="optionHover = null"
-              >
-                {{ index }}
-              </label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-6">
-              <label
-                class="m-0"
-                for="selectResultLanguage"
-                @mouseover="optionHover = 'resultLanguage'"
-                @mouseleave="optionHover = null"
-              >
-                Result Language:
-              </label>
-              <select
-                id="selectResultLanguage"
-                class="select input-small"
-                placeholder="Choose one"
-                v-model="options.resultLanguage"
-              >
-                <option
-                  v-for="language in languages"
-                  :key="language"
-                  :value="language"
-                  >{{ language }}</option
-                >
-              </select>
-            </div>
-            <div class="col-6">
-              <label
-                class="m-0"
-                for="selectQueryLanguages"
-                @mouseover="optionHover = 'queryLanguages'"
-                @mouseleave="optionHover = null"
-              >
-                Query Languages:
-              </label>
-              <select
-                id="selectQueryLanguages"
-                class="select input-small"
-                placeholder="Choose one"
-                @change="addQueryLanguage($event)"
-              >
-                <option :value="null">-- Select --</option>
-                <option
-                  v-for="language in languages"
-                  :key="language"
-                  :value="language"
-                  >{{ language }}</option
-                >
-              </select>
-              <div class="tag-container u-center mt-2">
-                <div
-                  class="tag tag--primary"
-                  v-for="language in options.queryLanguages"
-                  :key="language"
-                >
-                  {{ language }}
-                  <div
-                    class="tag tag__delete mt-1"
-                    @click="removeQueryLanguage(language)"
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <blockquote class="mx-1 mt-1 animated fadeIn" v-if="optionHover">
-            <span
-              ><strong>{{ optionHover }}:</strong>
-              {{ optionsUI[optionHover] }}
-            </span>
-          </blockquote>
-        </div>
+        <Options @options="getOptions" />
       </div>
       <div class="col-6 col-sm-12" id="data">
         <Code :code="code" />
@@ -169,36 +75,26 @@
 import { defineAsyncComponent } from "vue";
 import * as api from "@/assets/js/api.js";
 import * as axios from "axios";
-axios.defaults.headers.get["folder-type"] = "application/json";
+axios.defaults.headers.get["content-type"] = "application/json";
 
 export default {
   components: {
     JsonView: defineAsyncComponent(() => import("@/components/home/JsonView")),
     Code: defineAsyncComponent(() => import("@/components/home/Code")),
+    Options: defineAsyncComponent(() => import("@/components/home/Options")),
   },
   data() {
     return {
       data: null,
       query: "",
-      options: {
-        dumpResult: false,
-        matchAltNames: false,
-        matchAliases: false,
-        matchCategories: false,
-        verboseCategories: false,
-        resultLanguage: "English",
-        queryLanguages: ["English"],
-      },
+      options: null,
       folder: null,
       category: null,
       suggestion: null,
 
       code: null,
-
       dataLoad: false,
       queryDisabled: false,
-      optionHover: null,
-      languages: null,
     };
   },
   computed: {
@@ -208,25 +104,8 @@ export default {
     suggestions() {
       return require("@/assets/js/suggestions.js").default;
     },
-    optionsUI() {
-      return {
-        dumpResult:
-          "The query result will return an object with the properties: query, folder, match, options, filename, result.",
-        matchAltNames: "Allows the matching of alternate or custom names.",
-        matchAliases:
-          "Allows the matching of aliases. These are searchable fields that returns the data object the query matched in.",
-        matchCategories:
-          "Allows the matching of categories. If true, then returns an array if it matches.",
-        verboseCategories:
-          "Used if a category is matched. If true, then replaces each string name in the array with the data object instead.",
-        queryLanguages:
-          "Array of languages that your query will be searched in.",
-        resultLanguage: "Output language that you want your results to be in.",
-      };
-    },
   },
   mounted() {
-    this.getLanguages();
     if (!this.folder) this.folder = Object.keys(this.schema)[0];
     if (!this.category && this.folder)
       this.category = this.schema[this.folder][0];
@@ -236,23 +115,11 @@ export default {
     this.updateControls();
   },
   methods: {
-    getLanguages() {
-      this.dataLoad = true;
-      let url = api.getUrl("languages");
-      axios
-        .get(url)
-        .then((response) => (this.languages = response.data))
-        .catch((error) => {
-          console.log(error);
-          this.dataLoad = false;
-        })
-        .finally(() => {
-          this.dataLoad = false;
-        });
-    },
     getData() {
       this.dataLoad = true;
+      console.log(this.options);
       let url = api.getUrl(this.folder, this.query, this.options);
+      console.log(url);
       axios
         .get(url)
         .then((response) => (this.data = response.data))
@@ -275,17 +142,21 @@ export default {
     },
     updateControls() {
       this.suggestion = this.suggestions[this.category];
+      if (this.options) {
+        this.options.matchCategories = true;
+      }
       if (this.suggestion) this.query = this.suggestion[0];
       else this.query = "";
+
       if (this.category == "all") {
         this.queryDisabled = true;
-        this.options.matchCategories = true;
         this.suggestion = null;
       } else if (this.category == "query") {
-        this.options.matchCategories = false;
+        if (this.options) {
+          this.options.matchCategories = false;
+        }
         this.queryDisabled = false;
       } else {
-        this.options.matchCategories = true;
         this.queryDisabled = false;
       }
     },
@@ -297,36 +168,14 @@ export default {
       this.suggestion = this.suggestions[event.target.value];
       this.updateControls();
     },
-    setOptionInfo(text) {
-      if (text) {
-        this.optionHover = text;
-      } else {
-        this.optionHover = "";
-      }
-      console.log(text);
-    },
     setSuggestionToQuery(suggestion) {
       this.query = suggestion;
     },
-    addQueryLanguage(event) {
-      let language = event.target.value;
-      if (!(language == "-- Select --")) {
-        if (!this.options.queryLanguages.includes(language)) {
-          this.options.queryLanguages.push(language);
-        }
-      }
-    },
-    removeQueryLanguage(language) {
-      if (this.options.queryLanguages.includes(language)) {
-        this.options.queryLanguages.pop(language);
-      }
+    getOptions(options) {
+      this.options = options;
     },
   },
 };
 </script>
 
-<style>
-.tooltip {
-  white-space: pre-line;
-}
-</style>
+<style></style>
