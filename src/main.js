6cf3e163f2fa8117e7828778625f3b05e2324329
Sephalias <vinceliboon91@gmail.com>
2023-08-24 10:10:47 +0800

    Change css framework from cirrus to naiveui

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "cirrus-ui";
+import "vfonts/Lato.css"; // General Font
+import "vfonts/FiraCode.css"; // Monospace Font
 
 const app = createApp(App);
 
