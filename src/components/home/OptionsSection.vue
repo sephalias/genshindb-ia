12a6b4954d8c7465adab122ebef760d17eea68ca
Sephalias <vinceliboon91@gmail.com>
2023-10-25 21:56:21 +0800

    Fixed ui in production

--- src/components/home/OptionsSection.vue
+++ src/components/home/OptionsSection.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { computed, defineAsyncComponent, onMounted, ref } from "vue";
 import { getDefaultOptions, getLanguages } from "@/scripts/api";
 import { useOptionsStore } from "@/stores/options";
 import { Reload } from "@vicons/ionicons5";
 import { useNotification } from "naive-ui";
+import { computed, onMounted, ref } from "vue";
 
 const env = import.meta.env.MODE;
 
@@ -60,73 +60,71 @@ function resetOptions() {
 </script>
 
 <template>
-  <n-space id="options">
-    <n-space id="optionsDefault" vertical>
-      <n-divider>Options</n-divider>
-      <n-grid x-gap="12" cols="2 100:1 280:1 360:1 400:2">
-        <n-gi>
-          <n-space vertical>
-            <div
-              v-for="(value, index) in optionsStore"
-              :key="index"
-              :id="index.toString()"
-            >
-              <n-space v-if="typeof value == 'boolean'">
-                <n-switch v-model:value="optionsStore[index]" />
-                <n-tooltip trigger="hover">
-                  <template #trigger> {{ index }} </template>
-                  {{ (optionsDescriptions as any)[index] }}
-                </n-tooltip>
-              </n-space>
-            </div>
-          </n-space>
-        </n-gi>
-        <n-gi>
-          <n-space vertical>
-            <n-space vertical>
-              Result Language
-              <n-select
-                v-model:value="optionsStore['resultLanguage']"
-                virtual-scroll
-                :default-value="getDefaultOptions()['resultLanguage']"
-                :options="languageOptions"
-              />
+  <n-space id="options" vertical>
+    <n-divider>Options</n-divider>
+    <n-grid x-gap="12" cols="2 100:1 280:1 360:1 400:2">
+      <n-gi>
+        <n-space vertical>
+          <div
+            v-for="(value, index) in optionsStore"
+            :key="index"
+            :id="index.toString()"
+          >
+            <n-space v-if="typeof value == 'boolean'">
+              <n-switch v-model:value="optionsStore[index]" />
+              <n-tooltip trigger="hover">
+                <template #trigger> {{ index }} </template>
+                {{ (optionsDescriptions as any)[index] }}
+              </n-tooltip>
             </n-space>
-            <n-space vertical>
-              Query Language
-              <n-select
-                v-model:value="optionsStore['queryLanguages']"
-                multiple
-                clearable
-                :default-value="getDefaultOptions()['queryLanguages']"
-                :options="languageOptions"
-              />
-            </n-space>
-            <n-button text type="info" @click="resetOptions">
-              <template #icon>
-                <n-icon>
-                  <Reload />
-                </n-icon>
-              </template>
-              Reset Options
-            </n-button>
-          </n-space>
-        </n-gi>
-      </n-grid>
-      <n-alert
-        title="Development Mode"
-        type="warning"
-        v-if="env === 'development'"
-      >
-        <n-space>
-          <n-code
-            language="json"
-            :code="JSON.stringify(optionsStore)"
-            word-wrap
-          />
+          </div>
         </n-space>
-      </n-alert>
-    </n-space>
+      </n-gi>
+      <n-gi>
+        <n-space vertical>
+          <n-space vertical>
+            Result Language
+            <n-select
+              v-model:value="optionsStore['resultLanguage']"
+              virtual-scroll
+              :default-value="getDefaultOptions()['resultLanguage']"
+              :options="languageOptions"
+            />
+          </n-space>
+          <n-space vertical>
+            Query Language
+            <n-select
+              v-model:value="optionsStore['queryLanguages']"
+              multiple
+              clearable
+              :default-value="getDefaultOptions()['queryLanguages']"
+              :options="languageOptions"
+            />
+          </n-space>
+          <n-button text type="info" @click="resetOptions">
+            <template #icon>
+              <n-icon>
+                <Reload />
+              </n-icon>
+            </template>
+            Reset Options
+          </n-button>
+        </n-space>
+      </n-gi>
+    </n-grid>
+    <n-alert
+      title="Development Mode"
+      type="warning"
+      v-if="env === 'development'"
+    >
+      <n-space>
+        <n-code
+          language="json"
+          :code="JSON.stringify(optionsStore)"
+          word-wrap
+        />
+      </n-space>
+    </n-alert>
   </n-space>
 </template>
 
